50b627c632d92e3cefb05df9b37e8eff7b93c27d
alexrauscher <alexanderrauscher@gmx.at>
2016-05-03 19:34:38 +0200

    virtuelle Eigenschaften hinzugefügt

--- Crowdfunding-Server_Angabe/src/app/model/Beteiligung.java
+++ Crowdfunding-Server_Angabe/src/app/model/Beteiligung.java
@@ -1,8 +1,17 @@
 package app.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.constraints.Pattern;
+
+import org.hibernate.annotations.Formula;
+
 import app.model.Projekt;
 
 @Entity
@@ -13,4 +22,54 @@ public class Beteiligung extends Persistent{
 
 	@ManyToOne
 	private Projekt projekt;
+	
+	private Integer betrag;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date datum;
+	
+	@Formula("select sum(b.betrag) from beteiligung b where b.projekt_id = projekt_id and b.datum <= datum")
+	@Transient
+	@Column(updatable = false)
+	private Integer bisherBetrag;
+
+	public String getSponsor() {
+		return sponsor;
+	}
+
+	public void setSponsor(String sponsor) {
+		this.sponsor = sponsor;
+	}
+
+	public Projekt getProjekt() {
+		return projekt;
+	}
+
+	public void setProjekt(Projekt projekt) {
+		this.projekt = projekt;
+	}
+
+	public Integer getBetrag() {
+		return betrag;
+	}
+
+	public void setBetrag(Integer betrag) {
+		this.betrag = betrag;
+	}
+
+	public Date getDatum() {
+		return datum;
+	}
+
+	public void setDatum(Date datum) {
+		this.datum = datum;
+	}
+
+	public Integer getBisherBetrag() {
+		return bisherBetrag;
+	}
+
+	public void setBisherBetrag(Integer bisherBetrag) {
+		this.bisherBetrag = bisherBetrag;
+	}
 }
--- Crowdfunding-Server_Angabe/src/app/model/Projekt.java
+++ Crowdfunding-Server_Angabe/src/app/model/Projekt.java
@@ -3,11 +3,15 @@ package app.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.constraints.Pattern;
+
+import org.hibernate.annotations.Formula;
 
 @Entity
 public class Projekt extends Persistent{
@@ -18,8 +22,74 @@ public class Projekt extends Persistent{
 	@Lob
 	private String grafik;
 	
-	private int zielBetrag;
+	private Integer zielBetrag;
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date ende;
+	
+	@Formula("timestampdiff('day', now(), ende)")
+	@Transient
+	@Column(updatable = false)
+	private Integer restLaufzeit;
+	
+	@Transient
+	@Column(updatable = false)
+	private boolean beendet;
+
+	public String getBeschreibung() {
+		return beschreibung;
+	}
+
+	public void setBeschreibung(String beschreibung) {
+		this.beschreibung = beschreibung;
+	}
+
+	public String getGrafik() {
+		return grafik;
+	}
+
+	public void setGrafik(String grafik) {
+		this.grafik = grafik;
+	}
+
+	public Integer getZielBetrag() {
+		return zielBetrag;
+	}
+
+	public void setZielBetrag(Integer zielBetrag) {
+		this.zielBetrag = zielBetrag;
+	}
+
+	public Date getEnde() {
+		return ende;
+	}
+
+	public void setEnde(Date ende) {
+		this.ende = ende;
+	}
+
+	public Integer getRestLaufzeit() {
+		return restLaufzeit;
+	}
+
+	public void setRestLaufzeit(Integer restLaufzeit) {
+		this.restLaufzeit = restLaufzeit;
+		wurdeBeendet(restLaufzeit);
+	}
+
+	public boolean isBeendet() {
+		return beendet;
+	}
+
+	public void setBeendet(boolean beendet) {
+		this.beendet = beendet;
+	}
+	
+	private void wurdeBeendet(int restLaufzeit) {
+		if(restLaufzeit > 0){
+			beendet = false;
+		}else{
+			beendet = true;
+		}
+	}
 }
